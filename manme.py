520cbde597d5a47cd0fc0861282b29fc321fb6d0
ALEX <brawlleonak@gmail.com>
2023-01-21 14:55:54 +0200

    Version 0.2

--- manme.py
+++ manme.py
@@ -1,1 +1,15 @@
-print('test')
+class Car:
+    name = 'bugatti'
+    wheels = 4
+    speed = 120
+    color = 'Green'
+    power = 85
+
+print(Car.name)
+print(Car.wheels)
+print(Car.speed)
+print(Car.power)
+print(Car.color)
+
+class Table:
+    height = 90
