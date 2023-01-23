ddf9737974efdd0f90923793ddd2a0041072c66c
ALEX <brawlleonak@gmail.com>
2023-01-23 12:31:25 +0200

    Version 1

---
+++ vvcvc.py
@@ -0,0 +1,47 @@
+class Car:
+    name = 'name:bugatti'
+    wheels = 'wheels:4'
+    speed = 'speed:215'
+    color = 'Color:green'
+    power = 'power:85'
+    pon = '------------------'
+
+print(Car.name)
+print(Car.wheels)
+print(Car.speed)
+print(Car.power)
+print(Car.color)
+print(Car.pon)
+
+class Table:
+    name = 'name:folding table'
+    weight = 'weight:90'
+    width ='width:50 centimeter'
+    height = 'height:67'
+    color = 'Color:Black'
+    pon = '------------------'
+
+
+print(Table.name)
+print(Table.weight)
+print(Table.width)
+print(Table.height)
+print(Table.color)
+print(Table.pon)
+
+
+class Cat:
+    breed = 'Breed:Maine Coon'
+    color = 'Color:orange'
+    paws = 'Paws:4'
+    wool = 'Wool:Fluffy'
+    Character = 'Character:Playful'
+    pon = '---------------'
+
+print(Cat.breed)
+print(Cat.color)
+print(Cat.paws)
+print(Cat.wool)
+print(Cat.Character)
+print(Cat.pon)
+
